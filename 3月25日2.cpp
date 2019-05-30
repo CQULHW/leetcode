d93873033410880328439af36cf0fc1fe7c9786b
CQULHW <45911839+CQULHW@users.noreply.github.com>
2019-05-30 23:28:52 +0800

    Update 3月25日2.cpp

--- 3月25日2.cpp
+++ 3月25日2.cpp
@@ -1,3 +1,4 @@
+方法一：
 #include <iostream>
 
 using namespace std;
@@ -18,3 +19,51 @@ int main(int argc, char* argv[])
     system("pause");
     return 0;
 } 
+
+
+方法二：
+#include<iostream>
+#include<string.h>
+using namespace std;
+void SwapPointer(char *p,char*q)
+{
+   char temp;
+   int n1=strlen(p);
+   int n2=strlen(q);
+ 
+   if(n1>n2)
+   {
+	   for(int i=0;i<n2;i++)
+	   {
+		   temp=p[i];
+		   p[i]=q[i];
+		   q[i]=temp;
+	   }
+	   for(int i=n2;i<n1;i++)
+	   {
+		   q[i]=p[i];
+	       p[i]='\0';
+	   }
+   }
+   else
+   {
+	    for(int i=0;i<n1;i++)
+	   {
+		   temp=p[i];
+		   p[i]=q[i];
+		   q[i]=temp;
+	   }
+	   for(int i=n1;i<n2;i++)
+	   {
+		   p[i]=q[i];
+	       q[i]='\0';
+	   }
+   }
+}
+int main(){
+	char *gm="good morning";
+	char *gf="good afternoon";
+	cout<<"修改前"<<"gm="<<gm<<" "<<"gf="<<gf<<endl;
+	swap(gm,gf);
+	cout<<"修改前"<<"gm="<<gm<<" "<<"gf="<<gf<<endl;
+}
